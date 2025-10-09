d4579c2aaefc4f4e052f5db686aaa9e6179c6759
Hongyang Zhao <hongyang.zhao@thundersoft.com>
2025-10-09 19:19:16 +0800

    input: touchscreem: cap1296: Add touch pack driver
    The CAP1296 is a multiple channel capacitive touch sensor controller,
    support five buttons on 360-came.

--- drivers/input/touchscreen/cap1296.c
+++ drivers/input/touchscreen/cap1296.c
@@ -1,3 +1,8 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ */
+
 #include <linux/kernel.h>
 #include <linux/delay.h>
 #include <linux/interrupt.h>
--- drivers/input/touchscreen/cap1296.h
+++ drivers/input/touchscreen/cap1296.h
@@ -1,3 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ */
+
 #ifndef _LINUX_INPUT_CAP1296_H_
 #define _LINUX_INPUT_CAP1296_H_
 
